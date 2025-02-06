db2c5123c41c3cac478cbd2a14d576f4895ce42f
AlionaKN <71887273+AlionaKN@users.noreply.github.com>
2025-02-06 11:52:29 +0200

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,3 +1,18 @@
+"""
+Data Types. Lists. Task 1
+Write a Python program that accepts a sequence of words as input and prints the unique words in a sorted form.
+
+Examples:
+
+Input:
+
+('red', 'white', 'black', 'red', 'green', 'black') 
+Output:
+
+['black', 'green', 'red', 'white']
+"""
+
+
 from typing import List, Tuple
 
 def sort_unique_elements(str_list: Tuple[str]) -> List[str]:

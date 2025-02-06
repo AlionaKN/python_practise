a3b6089c051cf037b96c788c580b0dfa7cd7a783
AlionaKN <71887273+AlionaKN@users.noreply.github.com>
2025-02-06 11:50:47 +0200

    Add files via upload

---
+++ task_2.py
@@ -0,0 +1,13 @@
+from typing import List, Tuple
+
+def sort_unique_elements(str_list: Tuple[str]) -> List[str]:
+    unique_list = []
+
+    for i in str_list:
+        if i not in unique_list:
+            unique_list.append(i)
+
+    unique_list.sort()
+
+    return unique_list
+

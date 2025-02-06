f4b623a46738ffa2b79a960d9c40c0dcdb60c65a
AlionaKN <71887273+AlionaKN@users.noreply.github.com>
2025-02-06 11:53:51 +0200

    Update task.py

--- task.py
+++ task.py
@@ -1,3 +1,7 @@
+"""
+Update the get_fizzbuzz_list function. The function has to generate and return a list with numbers from 1 to n inclusive where the n is passed as a parameter to the function. But if the number is divided by 3 the function puts a Fizz word into the list, and if the number is divided by 5 the function puts a Buzz word into the list. If the number is divided by both 3 and 5 the function puts FizzBuzz into the list.
+"""
+
 from typing import Union, List
 
 ListType = List[Union[int, str]]
